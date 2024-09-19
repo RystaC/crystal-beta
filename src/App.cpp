ff9a341184a0a24ee71591ee8a92cdc0ecd7ae0a
RystaC <mirage.s.cpp@gmail.com>
2024-09-20 01:29:07 +0900

    add: add mouse control

--- src/App.cpp
+++ src/App.cpp
@@ -7,11 +7,16 @@ bool App::init(int window_width, int window_height) {
         return false;
     }
 
-    window_ = SDL_CreateWindow("[crystal-beta demo]", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_VULKAN);
+    window_ = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_VULKAN);
     if(!window_) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "[SDL2] %s\n", SDL_GetError());
         return false;
     }
+
+    window_width_ = window_width;
+    window_height_ = window_height;
+
+    SDL_ShowCursor(SDL_DISABLE);
 
     return true;
 }
